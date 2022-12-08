c03690a290815ac5ee25d39c502054d4fa09ca5d
Christopher Boyle <chrismjboyle@gmail.com>
2022-12-08 21:22:30 +0000

    aoc01 - Some tidying up

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -36,12 +36,9 @@ fn parse_lines_get_calories(lines: Lines<BufReader<File>>) -> Vec<u32>
     cals
 }
 
-fn main() {
-    let lines = read_lines("calories.csv").expect("foo");
-    let calories = parse_lines_get_calories(lines);
-    let mut indices: Vec<usize> = (0..calories.len()).collect();
-    indices.sort_by_key(|i| u32::MAX - calories[*i]);
-
+/// Part 1 - find the number of calories held by the elf carrying the most
+fn part1(calories: &Vec<u32>, indices: &Vec<usize>)
+{
     let top1 = indices[0];
     let top1_calories = calories[top1];
     println!("Part 1");
@@ -49,8 +46,24 @@ fn main() {
         "The elf with the most calories is #{} with {} calories.",
         top1, top1_calories
     );
+}
 
+/// Part 2 - find the total calories held by top three elves
+fn part2(calories: &Vec<u32>, indices: &Vec<usize>)
+{
     let top3_calories: u32 = indices[0..3].iter().map(|i| calories[*i]).sum();
     println!("Part 2");
     println!("The top 3 elves have, in total, {top3_calories} calories.");
+}
+
+fn main() {
+    let lines = read_lines("calories.csv").expect("foo");
+    let calories = parse_lines_get_calories(lines);
+    let mut indices: Vec<usize> = (0..calories.len()).collect();
+
+    // sort indices in descending order of calories held
+    indices.sort_by_key(|i| u32::MAX - calories[*i]);
+
+    part1(&calories, &indices);
+    part2(&calories, &indices);
 }
